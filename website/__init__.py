b3e2c61ae86591826118870cca4db72f4536bcaf
Nagaprasadvr@#£ <nagaprasadvr246@gmail.com>
2021-12-11 11:58:08 +0530

    sign-up formatting

--- website/__init__.py
+++ website/__init__.py
@@ -1,4 +1,5 @@
 from flask import Flask
+import secrets
 from flask_sqlalchemy import SQLAlchemy
 from os import path
 db = SQLAlchemy()
@@ -6,14 +7,18 @@ DB_NAME = "database.db"
 def create_app():
 
     app = Flask(__name__)
-    app.config['SECRET KEY'] = "1m9n3gs5sa6j4sjf7h8hu829739kdh"
+    #app.config['SESSION_TYPE'] = 'filesystem'
+    secret = secrets.token_urlsafe(32)
+
+    app.secret_key = secret
     app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
     db.init_app(app)
     from .views import views
     from .auth import auth
     app.register_blueprint(views, url_prefix ='/')
     app.register_blueprint(auth, url_prefix ='/')
-    from .models import User, Blood_and_plasma
+
+    from website import models
     create_database(app)
 
     return app
